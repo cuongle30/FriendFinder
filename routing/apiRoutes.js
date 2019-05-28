04cb0a2e8da7c3503543d835166cbf1e951e53a7
cuongle30 <cuongle30@gmail.com>
2019-05-28 16:02:01 -0700

    adding apiroutes

---
+++ routing/apiRoutes.js
@@ -0,0 +1,45 @@
+var friends = require("../data/friends");
+
+module.exports = function(app) {
+  // Get all friends found in friends.js
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body.scores);
+
+    // Getting user info
+    var user = req.body;
+
+    for(var i = 0; i < user.scores.length; i++) {
+      user.scores[i] = parseInt(user.scores[i]);
+    }
+
+    // default friend match is the first friend but result will be whoever has the minimum difference in scores
+    var bestMatch = 0;
+    var minimumDifference = 40;
+
+    // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
+    //  whatever the difference is, add to the total difference
+    for(var i = 0; i < friends.length; i++) {
+      var totalDifference = 0;
+      for(var j = 0; j < friends[i].scores.length; j++) {
+        var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+        totalDifference += difference;
+      }
+
+      // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+      if(totalDifference < minimumDifference) {
+        bestMatch = i;
+        minimumDifference = totalDifference;
+      }
+    }
+
+    // after finding match, add user to friend array
+    friends.push(user);
+
+    // send back to browser the best friend match
+    res.json(friends[bestMatch]);
+  });
+};
