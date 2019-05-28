d817c2793ee6af76ccff5093cb93ec4efda0ee74
cuongle30 <cuongle30@gmail.com>
2019-05-28 16:34:47 -0700

    update apirroutes

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -19,9 +19,8 @@ module.exports = function(app) {
     // default friend match is the first friend but result will be whoever has the minimum difference in scores
     var bestMatch = 0;
     var minimumDifference = 40;
-
-    // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
-    //  whatever the difference is, add to the total difference
+  
+    //Creating loop for difference in positive
     for(var i = 0; i < friends.length; i++) {
       var totalDifference = 0;
       for(var j = 0; j < friends[i].scores.length; j++) {
@@ -29,17 +28,16 @@ module.exports = function(app) {
         totalDifference += difference;
       }
 
-      // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
       if(totalDifference < minimumDifference) {
         bestMatch = i;
         minimumDifference = totalDifference;
       }
     }
 
-    // after finding match, add user to friend array
+    // Adding user to friend array
     friends.push(user);
 
-    // send back to browser the best friend match
+    //Showing the best match
     res.json(friends[bestMatch]);
   });
 };
