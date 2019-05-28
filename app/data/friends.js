0e422e587d9cc9f042f1e1c24fd16ef9f4bd681f
cuongle30 <cuongle30@gmail.com>
2019-05-28 13:32:57 -0700

    add friend list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,28 @@
 //Array of friends in data
-var friendsArray = {
+var friendsArray = [{
     "name":"Ahmed",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+    "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    "name":"Chris",
+    "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Jon_Stock_2015.jpg/1200px-Jon_Stock_2015.jpg",
+    "scores":[4, 3, 1, 4, 4, 2, 2, 2, 3, 1]
+  },
+  {
+    "name":"Kim",
+    "photo":"https://images.pexels.com/photos/1036622/pexels-photo-1036622.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "scores":[1, 4, 2, 1, 4, 2, 2, 5, 4, 5]
+  },
+  {
+    "name":"Ashley",
+    "photo":"https://llizo.marketing/wp-content/uploads/2019/03/best-practices-stock-photos-woman-eating-salad.jpg",
+    "scores":[3, 1, 4, 5, 4, 1, 5, 1, 2, 1]
+  },
+  {
+    "name":"Joey",
+    "photo":"https://images.pexels.com/photos/555790/pexels-photo-555790.png?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "scores":[1, 5, 2, 2, 1, 5, 3, 5, 4, 3]
   }
-  
+]
+module.exports = friends;
